488e670e2c2bf45d78c6fa30eb95e52ce59346c6
Kyriakos Chalvatzis <kyriakos.halvatzis@gmail.com>
2023-11-05 16:19:41 +0200

    commit new branch

--- product_service/main.go
+++ product_service/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Print("Welcome to the Product Service.\n")
+
+	// Load environment variables using godotenv
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Printf("PORT: %v\n", portString)
+
+}
